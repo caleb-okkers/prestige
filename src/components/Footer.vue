73f248edda7c6f56db3c8f7ab1d5c36247841d50
caleb-okkers <okkerscaleb@gmail.com>
2024-09-06 14:18:37 +0200

    comps

---
+++ src/components/Footer.vue
@@ -0,0 +1,36 @@
+<template>
+    <footer class="container-fluid">
+        <div class="row">
+            <p class="lead">The Hotel &copy; <span class="shadow">{{ cYear }}</span></p>
+        </div>
+    </footer>
+</template>
+<script>
+export default {
+    name: "FooterComp",
+    data() {
+        return {
+            cYear: new Date().getUTCFullYear()
+        }
+    }
+}
+</script>
+<style scoped>
+
+footer {
+    color: #000;
+    background: #fff;
+    font-family: "Bona Nova SC", serif;
+    font-weight: 500;
+    font-style: normal;
+    font-size: 2.5rem;
+    color: rgb(29, 29, 29);
+    bottom: 0;
+    z-index: 1;
+}
+
+footer p {
+    padding-top: 2rem;
+}
+    
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="home">
+    <div class="video-container">
+
+      <video class="video" autoplay muted preload="auto" loop="loop" playsinline data-poster="/globalassets/hotel-new-york/images/pre-opening-cgis/2021-05-01_20-e-76th_nicholas-venezia_01-copy.jpg?width=1100&amp;height=700&amp;rxy=0.5%2c0.5&amp;rmode=crop&amp;quality=90">
+        <source src="/globalassets/corinthia-global/videos/homepage-videos/new-york---final.mp4" type="video/mp4">
+      </video>
+    </div>
   </div>
 </template>
 
@@ -11,3 +17,26 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+
+.video-container {
+    position: relative;
+}
+
+.video{
+    position: absolute;
+    width: 100%;
+    height: 100%;
+}
+
+.video-container:after {
+    content: "";
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    background: linear-gradient(0deg, transparent 50%, #1F1D19 100%);
+}
+</style>
