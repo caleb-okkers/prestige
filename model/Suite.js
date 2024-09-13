3b1924ee03b7b25c0b0ed1ce3a379e13e220bc20
caleb-okkers <okkerscaleb@gmail.com>
2024-09-13 13:12:20 +0200

    debugging

--- model/Suite.js
+++ model/Suite.js
@@ -1,7 +1,7 @@
 import { connection as db } from '../config/index.js';
 
 class Suites {
-    async fetchSuites(req, res) {
+     fetchSuites(req, res) {
         try {
             const strQry = `
                 SELECT suite_id, suite_name, suite_number, suite_type, description, price_per_night, max_occupancy, availability_status, img_url 
@@ -22,7 +22,7 @@ class Suites {
         }
     }
 
-    async fetchSuite(req, res) {
+     fetchSuite(req, res) {
         try {
             const { id } = req.params;
             const strQry = `
@@ -48,30 +48,7 @@ class Suites {
         }
     }
 
-    async premiumSuites(req, res) {
-        try {
-            const strQry = `
-                SELECT suite_id, suite_name, suite_number, suite_type, description, price_per_night, max_occupancy, availability_status, img_url 
-                FROM suites 
-                ORDER BY price_per_night DESC 
-                LIMIT 3
-            `;
-            db.query(strQry, (err, results) => {
-                if (err) throw new Error(err.message);
-                res.json({
-                    status: res.statusCode,
-                    results
-                });
-            });
-        } catch (e) {
-            res.status(500).json({
-                status: 500,
-                err: e.message
-            });
-        }
-    }
-
-    async addSuite(req, res) {
+     addSuite(req, res) {
         try {
             const data = req.body;
             const strQry = `INSERT INTO suites SET ?`;
@@ -90,7 +67,7 @@ class Suites {
         }
     }
 
-    async updateSuite(req, res) {
+     updateSuite(req, res) {
         try {
             const { id } = req.params;
             const data = req.body;
@@ -114,7 +91,7 @@ class Suites {
         }
     }
 
-    async deleteSuite(req, res) {
+     deleteSuite(req, res) {
         try {
             const { id } = req.params;
             const strQry = `DELETE FROM suites WHERE suite_id = ?`;
